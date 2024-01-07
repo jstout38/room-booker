a561e626b8d86d4b0bf3b1be01dac17a61905ccc
jstout38 <jstout38@gmail.com>
2024-01-06 21:09:42 -0500

    delete functionality added

--- src/Calendar.js
+++ src/Calendar.js
@@ -68,6 +68,10 @@ export const Calendar = () => {
 
   const [ statsState, setStatsState ] = useState();
 
+  const [ showConfirmModal, setShowConfirmModal ] = useState(false);
+
+  const [ currentEvent , setCurrentEvent ] = useState();
+
   let gapiInited = false,
   gisInited = false,
   tokenClient; 
@@ -243,9 +247,33 @@ export const Calendar = () => {
       }
     )
     setShowEditModal(false);
+  }
 
-      
-    
+function handleDelete() {
+  var request = gapi.client.calendar.events.delete({
+    calendarId: RoomIDs[currentRoom],
+    eventId: currentEvent,
+  });
+  request.execute(
+    async (event) => {
+      var newEvents = {};
+      for (var key of Object.keys(RoomIDs)) {
+        var events = await listUpcomingEvents(key);
+        newEvents[key] = events;        
+      }
+      setCurrentEvents(newEvents);
+    },
+    (error) => {
+      handleSignoutClick();
+    }
+  );
+  setShowConfirmModal(false);
+}
+
+function confirmDelete(roomId, id) {  
+  setCurrentRoom(roomId);
+  setCurrentEvent(id);
+  setShowConfirmModal(true);
 }
 
   
@@ -293,12 +321,20 @@ export const Calendar = () => {
             {event.description ? "Notes: " + event.description : ""}
           </div>
         </div>
-        <div className="col-span-1 row-start-1 col-start-8">
-        <svg onClick={() => handleEdit(id, event.id)} className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 20 18">
+        <span className="col-span-1 row-start-1 col-start-8 grid grid-cols-subgrid">
+        <div>
+        <svg onClick={() => confirmDelete(id, event.id)} className="p-1 w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
+        </svg>
+        </div>
+        <div>
+        <svg onClick={() => handleEdit(id, event.id)} className="p-1 w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 20 18">
           <path d="M12.687 14.408a3.01 3.01 0 0 1-1.533.821l-3.566.713a3 3 0 0 1-3.53-3.53l.713-3.566a3.01 3.01 0 0 1 .821-1.533L10.905 2H2.167A2.169 2.169 0 0 0 0 4.167v11.666A2.169 2.169 0 0 0 2.167 18h11.666A2.169 2.169 0 0 0 16 15.833V11.1l-3.313 3.308Zm5.53-9.065.546-.546a2.518 2.518 0 0 0 0-3.56 2.576 2.576 0 0 0-3.559 0l-.547.547 3.56 3.56Z"/>
           <path d="M13.243 3.2 7.359 9.081a.5.5 0 0 0-.136.256L6.51 12.9a.5.5 0 0 0 .59.59l3.566-.713a.5.5 0 0 0 .255-.136L16.8 6.757 13.243 3.2Z"/>
         </svg>
         </div>
+        
+        </span>
       </div>
       )
     });
@@ -671,6 +707,45 @@ export const Calendar = () => {
                     Edit
                   </button>
                 <button onClick={() => setShowEditModal(false)} type="button" className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div 
+  id="edit-modal" 
+  tabIndex="-1" 
+  aria-hidden="true" 
+  className=
+  {classNames('overflow-y-auto overflow-x-hidden absolute top-1/4 left-1/3 w-1/2 z-50 justify-center items-center max-h-full',
+  {'hidden' : !showConfirmModal})}
+>
+    <div className="relative p-4 w-full max-w-2xl max-h-full">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                    Confirm Delete
+                </h3>
+                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" onClick={() => setShowConfirmModal(false)}>
+                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                </button>
+            </div>
+            <div className="p-4 bg-red-700 text-4xl">
+                Are you sure?
+            </div>
+            
+            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button 
+                  onClick={handleDelete} 
+                  disabled={inputControl.name.length === 0 || (((Number(inputControl.startTime.slice(0,2))) > Number(inputControl.endTime.slice(0,2))) || (Number(inputControl.startTime.slice(0,2)) >= Number(inputControl.endTime.slice(0,2)) && (Number(inputControl.startTime.slice(3,5)) > Number(inputControl.endTime.slice(3,5)))))} 
+                  data-modal-hide="default-modal" 
+                  type="button" 
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-25">
+                    Delete
+                  </button>
+                <button onClick={() => setShowConfirmModal(false)} type="button" className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
             </div>
         </div>
     </div>
